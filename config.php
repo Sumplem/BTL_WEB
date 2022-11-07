eede94ac83bafd7a4b016b6acd4d848c915dccc7
Sumplem <lucgiahung310@gmail.com>
2022-11-07 16:36:45 +0700

    commit

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('_HOST','localhost');
 define('_USER_NAME','root');
 define('_PASSWORD','');
 define('_DATABASE','btlweb');
-define('_DIR','NetworkComputer_Ass1');
+define('_DIR','BTL_WEB');
 
 $root = 'http://'.$_SERVER['HTTP_HOST'].'/'._DIR;
 define('_WEB_ROOT',$root);
